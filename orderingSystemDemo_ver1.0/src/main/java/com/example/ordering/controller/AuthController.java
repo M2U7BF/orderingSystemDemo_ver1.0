57e6e27d86d95ce97aba88648cf7e81f87c95c5e
M2U7BF <yhvh4321@gmail.com>
2022-07-23 17:06:41 +0900

    [追加]認証のViewを作成

--- orderingSystemDemo_ver1.0/src/main/java/com/example/ordering/controller/AuthController.java
+++ orderingSystemDemo_ver1.0/src/main/java/com/example/ordering/controller/AuthController.java
@@ -1,5 +1,36 @@
 package com.example.ordering.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
 public class AuthController {
 
+    @GetMapping("login")
+    public String login(){
+        return "auth/login";
+    }
+
+    @PostMapping("login{verify}")
+    public String loginVerify(){
+        return "redirect:/service";
+    }
+    
+    @GetMapping("createAccount")
+    public String createAccount(){
+        return "auth/createAccount";
+    }
+
+    @GetMapping("logout")
+    public String logout(){
+        return "auth/logout";
+    }
+
+    @GetMapping("passwordReset")
+    public String passwordReset(){
+        return "auth/passwordReset";
+    }
 }
